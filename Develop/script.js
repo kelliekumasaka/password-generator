c60b5b04b630f80cff5400027cdf0132e9098662
kelliekumasaka <kelliek3@gmail.com>
2021-09-21 21:47:33 -0700

    pw arrays and basic writepassword function

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,31 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var userNumber, smallAlphabet, bigAlphabet, numbers, differentCharacters;
+var userInput = []
+var numeric = ["1", "2","3","4","5","6","7","8","9","0" ]
+var lowercase = ["a",'b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+var uppercase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+var specialCharacters = ['!','"','#','$','%','&',"'",'(',')','*','+',',','-','.','/',':',';','<','>',"=",'?','@','[',']','^','_','`','{','}',"|","~"]
 
 // Write password to the #password input
 function writePassword() {
+  userAnswers ();
+  if(smallAlphabet) {
+    userInput = userInput.concat(lowercase);
+  } 
+  if(bigAlphabet) {
+    userInput = userInput.concat(uppercase);
+  }
+  if(numbers) {
+    userInput = userInput.concat(numeric);
+  }
+  if(differentCharacters) {
+    userInput = userInput.concat(specialCharacters)
+  }
+  for (let i = 0; i < userNumber; i++){
+    console.log(Math.floor(Math.random() * userInput.length))
+  }
+ 
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
@@ -10,5 +33,54 @@ function writePassword() {
 
 }
 
+function userAnswers() {
+  userNumber = prompt("How many characters in password? Enter a number between 8 and 128");
+  smallAlphabet = confirm("Would you like to include lowercase letters?")
+  bigAlphabet = confirm("Would you like to include uppercase letters?")
+  numbers = confirm("Would you like to include numbers?")
+  differentCharacters = confirm("Would you like to include special characters?")
+}
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+// console.log driven development
+
+// GIVEN I need a new, secure password
+// WHEN I click the button to generate a password
+// 🚩TODO: write the generatePassword function
+function generatePassword() {
+
+}
+
+// WHEN prompted for the length of the password
+// THEN I choose a length of at least 8 characters and no more than 128 characters
+// 🚩TODO: ask the user how many characters they want in password --> use a prompt which will be set to a var --> console.log(passwordlength) --> make sure the info is correct --> number --> 8+ or 128-
+
+
+// THEN I am presented with a series of prompts for password criteria
+// WHEN prompted for password criteria
+// THEN I select which criteria to include in the password
+// WHEN asked for character types to include in the password
+// THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+// 🚩TODO: write a series of 4 confirms --> set these to be a var --> to check if the user wants --> clo each variable from confirm
+// 🚩TODO: write 4 arrays, one for lowercase, uppercase, numeric, and special characters
+// 🚩TODO: check that the prompts true or false --> using above var for this --> and add associated arrays to a "holding array" upperConfirm --> true --> uppercase array --> add to holding array clo this
+
+
+// WHEN I answer each prompt
+// THEN my input should be validated and at least one character type should be selected
+// 🚩TODO: check that we have at lease one of the types
+
+
+// WHEN all prompts are answered
+// THEN a password is generated that matches the selected criteria
+// 🚩TODO: take our holding array and randomize the characters --> adding to a second array
+// 🚩TODO: take the randomized characters and select the amount that the user has chosen for the password length --> add these to a "return array" --> for loop the length for this will be the user password length clo this 3rd array to make sure that its the right length
+
+
+// WHEN the password is generated
+// THEN the password is either displayed written to the page
+// 🚩TODO: take our return array and use array methods to convert to a string --> have a var for this and return string clo this
