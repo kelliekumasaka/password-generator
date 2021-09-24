c3107f5cfa05a587573d32e68ec99c01422495c4
kelliekumasaka <kelliek3@gmail.com>
2021-09-23 18:08:19 -0700

    readme, commenting out on js, and clearpassword function creation

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # password-generator
+
+This assignment was to create a working password randomizer that would generate a string of characters based on the parameters that the user sets. In addition, this should be able to continue to loop, no matter how many times you wanted to generate a password, as well as break out of the loop if so desired.
+
+## Deployment
+
+My deployed version of the generator can be found [here](https://kelliekumasaka.github.io/password-generator/).
+
+## Contents
+
+The CSS is untouched, so the website itself should look the same as when received. When the "Generate" button is clicked, the following window pops up.
+
+![Password generator window with the question "How many characters in password? Enter a number between 8 and 128"](Assets/images/howmany.png)
+
+If you decide that you do not want a password, your window will show the following: 
+
+![Pop up that says "Goodbye!!"](Assets/images/goodbye.png)
+
+When you do want to have a password generated, after going through all of the prompts, your password will be generated!
+
+![Password generator page with random password in box](./assets/images/password.png)
+
+If you decide that you want another password made for you, your old password will stay, but will be replaced after going through all of the prompts.
+
+![Password generator page with old password and pop up asking whether or not user wants uppercase letters](./assets/images/questionwindow.png)
+
+## Summary
+
+This page is a successful deployment of a version of a random password generator. 
--- script.js
+++ script.js
@@ -1,30 +1,27 @@
-// Assignment Code
+// Defined necessary global variables and arrays
 var generateBtn = document.querySelector("#generate");
-var userNumber, smallAlphabet, bigAlphabet, numbers, specialAlphabet, numberError, characterError, underlineNine, password;
+var userNumber, smallAlphabet, bigAlphabet, numbers, specialAlphabet;
 var userInput = [];
 var numeric = ["1", "2","3","4","5","6","7","8","9","0" ];
 var lowercase = ["a",'b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 var uppercase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 var specialCharacters = ['!','"','#','$','%','&',"'",'(',')','*','+',',','-','.','/',':',';','<','>',"=",'?','@','[',']','^','_','`','{','}',"|","~"];
-userNumber = 0;
 var ask =true;
 var randomItems = [];
+var passwordText = document.querySelector("#password");
+userNumber = 0;
 
 // Write password to the #password input
 function writePassword() {
   getPasswordLength();
-
   for (let i = 0; i < userNumber; i++){
-    randomItems.push (userInput [Math.floor(Math.random() * userInput.length)]);
-    // console.log(randomItems);
-    // randomItems.push("#password")
+    randomItems.push (userInput[Math.floor(Math.random() * userInput.length)])
   }
-  console.log(randomItems)
   var stringItems = randomItems.join("");
-  var passwordText = document.querySelector("#password");
   passwordText.value = stringItems;
 }
 
+// Loop to ask the user for which characters to use
 function getUserInputs() {
   ask=true
   while (ask){
@@ -52,13 +49,14 @@ function getUserInputs() {
   }
 }
 
-
+// Confirm that user wants a password
 function getPasswordLength() {
   ask=true
+  // Loop to ask how long the password should be and to work out any edge cases
   while (ask){
     userNumber = prompt("How many characters in password? Enter a number between 8 and 128");
     if (userNumber === null){
-      alert("Goodbye");
+      alert("Goodbye!!");
       return;
     }else if(userNumber < 128 && userNumber > 8){
       ask=false;
@@ -71,43 +69,12 @@ function getPasswordLength() {
   }  
 }
 
+// Clears previously entered arrays
+function clearPassword(){
+  userInput.length=0;
+  randomItems.length=0;
+}
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-
-// console.log driven development
-
-// GIVEN I need a new, secure password
-// WHEN I click the button to generate a password
-// 🚩TODO: write the generatePassword function
-
-// WHEN prompted for the length of the password
-// THEN I choose a length of at least 8 characters and no more than 128 characters
-// 🚩TODO: ask the user how many characters they want in password --> use a prompt which will be set to a var --> console.log(passwordlength) --> make sure the info is correct --> IF number is not between 8 and 128, THEN alert and start over --> 8+ or 128-
-
-
-// THEN I am presented with a series of prompts for password criteria
-// WHEN prompted for password criteria
-// THEN I select which criteria to include in the password
-// WHEN asked for character types to include in the password
-// THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
-// 🚩TODO: write a series of 4 confirms --> set these to be a var --> to check if the user wants --> clo each variable from confirm
-// 🚩TODO: write 4 arrays, one for lowercase, uppercase, numeric, and special characters
-// 🚩TODO: check that the prompts true or false --> using above var for this --> and add associated arrays to a "holding array" upperConfirm --> true --> uppercase array --> add to holding array clo this
-
-
-// WHEN I answer each prompt
-// THEN my input should be validated and at least one character type should be selected
-// 🚩TODO: check that we have at lease one of the types
-
-
-// WHEN all prompts are answered
-// THEN a password is generated that matches the selected criteria
-// 🚩TODO: take our holding array and randomize the characters --> adding to a second array
-// 🚩TODO: take the randomized characters and select the amount that the user has chosen for the password length --> add these to a "return array" --> for loop the length for this will be the user password length clo this 3rd array to make sure that its the right length
-
-
-// WHEN the password is generated
-// THEN the password is either displayed written to the page
-// 🚩TODO: take our return array and use array methods to convert to a string --> have a var for this and return string clo 
+// Add event listeners to generate button
+generateBtn.addEventListener("click", writePassword)
+generateBtn.addEventListener("click", clearPassword)
